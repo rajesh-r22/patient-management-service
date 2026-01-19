5b0afa51536f3bc50e0bbfe599a2dc8b3e730c10
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-01-19 18:12:07 +0530

    feat:Create repository package ,Add PatientRepository interface that extends Patient entity for interactiing with database

---
+++ patientService/src/main/java/com/patientService/patientService/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.patientService.patientService.repository;
+
+import com.patientService.patientService.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+}
