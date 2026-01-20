8628cb097b7c45281d49e5c9cd598defbbe8b1f6
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-01-21 04:30:12 +0530

    feat: Create method to convert PatientRequestDto to Entity/Model to save in database

--- patientService/src/main/java/com/patientService/patientService/mapper/PatientMapper.java
+++ patientService/src/main/java/com/patientService/patientService/mapper/PatientMapper.java
@@ -1,10 +1,14 @@
 package com.patientService.patientService.mapper;
 
+import com.patientService.patientService.dto.PatientRequestDto;
 import com.patientService.patientService.dto.PatientResponseDto;
 import com.patientService.patientService.model.Patient;
 
+import java.time.LocalDate;
+
 public class PatientMapper {
 
+//    hum client ko response dete hai responsedto ke form me, naki pura entity/model return kare it is danger, responsedto safe
     public static PatientResponseDto toDto(Patient patient){
         PatientResponseDto responseDto=new PatientResponseDto();
         responseDto.setId(patient.getId().toString());
@@ -13,5 +17,16 @@ public class PatientMapper {
         responseDto.setAddress(patient.getAddress());
         responseDto.setDateOfBirth(patient.getDateOfBirth().toString());
         return responseDto;
+    }
+
+//    jo client data post kiya hai usko Entity/model me convert kar re hai phir Database me save karenge
+    public static Patient toModel(PatientRequestDto patientRequestDto){
+        Patient patient=new Patient();
+        patient.setName(patientRequestDto.getName());
+        patient.setEmail(patientRequestDto.getEmail());
+        patient.setAddress(patientRequestDto.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDto.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDto.getRegisteredDate()));
+        return  patient;
     }
 }
