00f0012c639bbfc7dd04561514db3bc777fe5ad3
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-01-21 04:13:52 +0530

    feat: Add PatientRequestDto in dto package , as new patient will register through postmapping

---
+++ patientService/src/main/java/com/patientService/patientService/dto/PatientRequestDto.java
@@ -0,0 +1,30 @@
+package com.patientService.patientService.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PatientRequestDto {
+    @NotNull(message = "Name cannot not be blank")
+    @Size(max = 100,message ="Name must be  under 100 characters" )
+    private String name;
+
+    @NotNull(message = "email should be valid")
+    @Email
+    private String email;
+
+    @NotNull(message = "Address cannot be blank ")
+    private String address;
+
+    @NotNull(message = "Date-Of-Birth cannot be null")
+    private String dateOfBirth;
+
+    @NotNull(message = "Registered-Date should be valid")
+    private String registeredDate;
+}
