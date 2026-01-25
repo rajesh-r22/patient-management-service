9f855163e9da510d0e1ef6dab2d6d05f156347f8
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-01-25 05:46:42 +0530

    refactor:code cleanup remove unwanted import statement

--- patientService/src/main/java/com/patientService/patientService/dto/PatientResponseDto.java
+++ patientService/src/main/java/com/patientService/patientService/dto/PatientResponseDto.java
@@ -1,6 +1,5 @@
 package com.patientService.patientService.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
